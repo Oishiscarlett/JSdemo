2720a4fb36721c396d478a59a15e5fc4c0369ee9
Oishiscarlett <oishi4869@163.com>
2022-04-02 20:43:01 +0800

    feat:除动画效果，功能完成；因现版本的代码，动画效果不好添加，需要重构。

--- memory-card/script.js
+++ memory-card/script.js
@@ -2,10 +2,15 @@ const cardEl = document.getElementsByClassName("card-container")[0];
 const cardNumEl = document.getElementById("card-count");
 const addNewCardBtn = document.getElementById("add-new");
 const containerEl = document.getElementsByClassName("container")[0];
-const addBtn = document.getElementById("add-btn");
 const queEl = document.getElementById("que");
 const ansEl = document.getElementById('ans');
 const contentEl = document.getElementById('content');
+const addBtn = document.getElementById("add-btn");
+const delBtn = document.getElementById("delete-btn");
+const filpBtn = document.getElementById("filp-btn");
+const lBtn = document.getElementById("left-btn");
+const rBtn = document.getElementById("right-btn");
+const clearBtn = document.getElementById("clear-btn");
 
 
 let cardIndex = 1;
@@ -25,6 +30,19 @@ function setCardNum() {
     }
 }
 
+function showCard() {
+    cardSum++;
+    cardIndex = 1;
+    setCardNum();
+
+    contentEl.innerHTML = QAs[cardIndex - 1].que;
+}
+
+
+/**
+ * 添加监听
+ * 
+ */
 
 // 首页--添加新卡片按钮
 addNewCardBtn.addEventListener('click', e => {
@@ -45,16 +63,51 @@ addBtn.addEventListener('click', e => {
         QAs.push(obj);
         containerEl.style.display = 'block';
         showCard();
+        queEl.value = null;
+        ansEl.value = null;
     }
 });
 
-function showCard() {
-    cardSum++;
+// 返回上一页
+delBtn.addEventListener('click', e => {
+    containerEl.style.display = 'block';
+});
+
+// 点击反转卡片
+filpBtn.addEventListener('click', e => {
+    if (contentEl.innerHTML === QAs[cardIndex - 1].que) {
+        contentEl.innerHTML = QAs[cardIndex - 1].ans;
+    } else {
+        contentEl.innerHTML = QAs[cardIndex - 1].que;
+    }
+});
+
+// 上一张卡片
+lBtn.addEventListener('click', e => {
+    if (cardIndex - 1 > 0) {
+        cardEl.className = 'card-container left-active';
+        cardIndex--;
+        contentEl.innerHTML = QAs[cardIndex - 1].que;
+        cardNumEl.innerHTML = cardIndex + '/' + cardSum;
+        cardEl.className = 'card-container';
+    } 
+});
+
+// 下一张卡片
+rBtn.addEventListener('click', e => {
+    if (cardIndex + 1 <= cardSum) {
+        cardIndex++;
+        contentEl.innerHTML = QAs[cardIndex - 1].que;
+        cardNumEl.innerHTML = cardIndex + '/' + cardSum;
+    }
+});
+
+// 清空卡片
+clearBtn.addEventListener('click', e => {
+    QAs = [];
     cardIndex = 1;
+    cardSum = 0;
     setCardNum();
-
-    contentEl.innerHTML = QAs[cardIndex - 1].que;
-
-}
+});
 
 setCardNum();
